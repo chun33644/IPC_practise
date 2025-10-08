893c5ac222ea0fb4dc6d6f456ed888f86f2174c0
chun <chun19940713@gmail.com>
2025-10-08 18:12:14 +0800

    .

--- IPC_SOCK_v2/IPC_SOCK_general.c
+++ IPC_SOCK_v2/IPC_SOCK_general.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <sys/un.h>
@@ -7,10 +8,12 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
-#include "IPC_SOCK_common.h"
+#include "IPC_SOCK_general.h"
 #include "IPC_SOCK_config.h"
 
+/* mutex lock */
 int lock(pthread_mutex_t *mutex, const char *func_name) {
 
     int res = pthread_mutex_lock(mutex);
@@ -22,6 +25,8 @@ int lock(pthread_mutex_t *mutex, const char *func_name) {
     return 0;
 }
 
+
+/* mutex unlock */
 int unlock(pthread_mutex_t *mutex, const char *func_name) {
     int res = pthread_mutex_unlock(mutex);
     if (res != 0) {
@@ -32,24 +37,127 @@ int unlock(pthread_mutex_t *mutex, const char *func_name) {
     return 0;
 }
 
+
 /* closed thread and fd */
 int error_handler(client_info *list) {
 
     list->start_flag = 0;
     printf("Thread ready to join\n");
-    pthread_join(list->r_pid, NULL);
-    pthread_join(list->s_pid, NULL);
+    pthread_join(list->rev_pid, NULL);
+    pthread_join(list->sen_pid, NULL);
 
-    close(list->client.fd);
+    close(list->s_info.fd);
     memset(list, 0, sizeof(client_info));
 
     return 0;
 }
 
 
+/* find (in_use == false) from array for management list */
+client_info* add_member(client_info *list) {
+
+    for (int idx = 0; idx < CLI_MAX; idx ++) {
+
+        if (!list[idx].in_use) {
+            return &list[idx];
+        } else if (list[idx].in_use) {
+            continue;
+        }
+
+    }
+    return NULL;
+}
+
+
+/* release from management list */
+int release_member(int fd, client_info *list) {
+
+    for(int idx = 0; idx < CLI_MAX; idx ++) {
+
+        if (fd != list[idx].s_info.fd) {
+            continue;
+        } else {
+            list[idx].in_use = false;
+            printf("fd(%d) ready to release from list.\n", list->s_info.fd);
+            memset(&list[idx], 0, sizeof(list[idx]));
+            return 0;
+        }
+
+    }
+    printf("delete fail\n");
+    return -1;
+}
+
+
+/* setting fd flag (O_NONBLOCK) */
+int set_nonblocking(int fd) {
+
+    int flag = fcntl(fd, F_GETFL, 0);
+    if (flag < 0) {
+        perror("fcntl(F_GETFL)");
+        return -1;
+    }
+
+    if (fcntl(fd, F_SETFL, flag | O_NONBLOCK) == -1) {
+        perror("fcntl(F_SETFL)");
+        return -2;
+    }
+
+    return 0;
+}
+
+/* register epoll fd */
+int epoll_req(int *new_efd) {
+
+    *new_efd = epoll_create1(EPOLL_CLOEXEC);
+    if (new_efd < 0) {
+        perror("epoll_create");
+        return -1;
+    }
+    printf("epoll fd %d\n", *new_efd);
+
+    return 0;
+}
+
+/* epoll fd and listen fd add to interst list */
+int epoll_add(int epoll_fd, sock_info *info) {
+
+    info->ev.events = EPOLLIN | EPOLLET;
+    info->ev.data.fd = info->fd;
+
+    int c_res = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, info->fd, &info->ev);
+    if (c_res < 0) {
+        perror("epoll_ctl(EPOLL_CTL_ADD)");
+        close(info->fd);
+        return -1;
+    }
+
+    printf("fd(%d) added to epoll interst list.\n", info->fd);
+    return 0;
+}
+
+
+/* delete fd from interst list */
+int epoll_delete(int epoll_fd, struct epoll_event *close_ev) {
+
+    if (!close_ev->data.fd) {
+        printf("add to epoll not yet.\n");
+        return -1;
+    }
+
+    printf("fd(%d) ready to delete from epoll interst list.\n", close_ev->data.fd);
+    int d_res = epoll_ctl(epoll_fd, EPOLL_CTL_DEL, close_ev->data.fd, close_ev);
+    if (d_res < 0) {
+        perror("epoll_ctl(EPOLL_CTL_DEL)");
+        close(close_ev->data.fd);
+        return -2;
+    }
+
+    return 0;
+}
 
 /* defult : UDS connect */
-/* if want select IDS connect -> please '#define IDS_CONNECT' */
+/* if want to select IDS connect -> please '#define IDS_CONNECT' */
 int server_connect_init(sock_info *server) {
 
 #ifdef IDS_CONNECT
@@ -123,7 +231,7 @@ int server_connect_init(sock_info *server) {
 
 
 /* defult : UDS connect */
-/* if want select IDS connect -> please '#define IDS_CONNECT' */
+/* if want to select IDS connect -> please '#define IDS_CONNECT' */
 int client_connect_init(sock_info *client) {
 
 #ifdef IDS_CONNECT
--- IPC_SOCK_v2/IPC_SOCK_general.h
+++ IPC_SOCK_v2/IPC_SOCK_general.h
@@ -9,9 +9,17 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>
 
 #include "IPC_SOCK_config.h"
 
+/*
+typedef enum _error_code {
+    ERROR_EXCEEDED_CONNECTIONS_NUM = 1,
+
+
+} error_code;
+*/
 
 typedef union _sock_type {
     struct sockaddr_in ids;
@@ -23,27 +31,24 @@ typedef struct _sock_info {
     int fd;
     socklen_t len;
     union _sock_type addr;
+
+    //for epoll_monitor
+    struct epoll_event ev;
+
 } sock_info;
-
-
-typedef enum _error_code {
-    ERROR_EXCEEDED_CONNECTIONS_NUM = 1,
-
-
-} error_code;
 
 typedef struct _package {
     int header;
     char msg[MSG_MAX];
-    error_code err;
+    //error_code err;
 } package;
 
 
 typedef struct _client_info {
-    sock_info client;
+    sock_info s_info;
     package pkg;
-    pthread_t r_pid;
-    pthread_t s_pid;
+    pthread_t rev_pid;
+    pthread_t sen_pid;
     bool in_use;
     int start_flag;
 } client_info;
@@ -55,14 +60,41 @@ int lock(pthread_mutex_t *mutex, const char *func_name);
 /* mutex unlock */
 int unlock(pthread_mutex_t *mutex, const char *func_name);
 
+
+
 /* join thread & close fd & memset info */
 int error_handler(client_info *list);
+
+
+
+/* find (in_use == false) from array for management list */
+client_info* add_member(client_info *list);
+
+/* release from management list */
+int release_member(int fd, client_info *list);
+
+
+
+/* setting fd flag (O_NONBLOCK) */
+int set_nonblocking(int fd);
+
+/* register epoll fd */
+int epoll_req(int *new_efd);
+
+/* epoll fd and listen fd add to interst list */
+int epoll_add(int epoll_fd, sock_info *info);
+
+/* delete fd from interst list */
+int epoll_delete(int epoll_fd, struct epoll_event *close_ev);
+
 
 
 /* defult : UDS connect */
 /* if want select IDS connect -> please '#define IDS_CONNECT' */
 int server_connect_init(sock_info *server);
 int client_connect_init(sock_info *client);
+
+
 
 
 #endif
